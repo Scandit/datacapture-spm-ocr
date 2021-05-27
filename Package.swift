aadc5a6a726009a9d5729d92aaef82d03f86423f
Scandit <scandit@admiral.int.mirasense.com>
2021-05-27 17:44:02 +0200

    Updating ScanditOCR - v.6.8.0-beta.2

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "ScanditOCR", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-ocr-6.8.0-beta.1-xcframework.zip", checksum: "c271fc3c1d3fba666960f6e202b2be0a6d004b9c01a57184878e75882ab731bd")
+        .binaryTarget(name: "ScanditOCR", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-ocr-6.8.0-beta.2-xcframework.zip", checksum: "b04a57f46872f35e3808e9c78393ba24049f26c7b63156ddce2306b9789535cf")
     ]
 )
